9861594224a7be14fa59ec79035d7bbf4438e3b0
tanu611063 <65527789+tanu611063@users.noreply.github.com>
2020-07-20 23:07:51 +0900

    検索機能実装

--- app/models/word.rb
+++ app/models/word.rb
@@ -8,7 +8,7 @@ class Word < ApplicationRecord
   def self.search(search)
     # テーブルとのやり取りが必要なメソッドはモデルに記述
     if search
-      Word.where('title LIKE(?)', "％#{search}%")
+      Word.where('title LIKE(?)', "%#{search}%")
     # wordモデル内のtitleカラムでsearch(引数)の中身が含まれていたらwhereで取得。％#{search}%の為、引数の１文字でも含まれていたら取得できる
     else
       Word.all
